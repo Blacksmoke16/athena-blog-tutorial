8d6c595d6262bed3d88233383d9831caf2d41728
Blacksmoke16 <yomoejoe@gmail.com>
2019-11-24 09:34:31 -0500

    Don't use converter FQN

--- src/controllers/article_controller.cr
+++ src/controllers/article_controller.cr
@@ -6,7 +6,7 @@ module Blog::Controllers
     def initialize(@request_stack : Athena::Routing::RequestStack, @user_storage : UserStorage); end
 
     @[Athena::Routing::Post(path: "")]
-    @[Athena::Routing::ParamConverter(param: "body", type: Blog::Models::Article, converter: Athena::Routing::Converters::RequestBody)]
+    @[Athena::Routing::ParamConverter(param: "body", type: Blog::Models::Article, converter: RequestBody)]
     def new_article(body : Blog::Models::Article) : Blog::Models::Article
       # Sets the owner of the blog post as the current authed user
       body.user = @user_storage.user
@@ -22,7 +22,7 @@ module Blog::Controllers
     end
 
     @[Athena::Routing::Put(path: "")]
-    @[Athena::Routing::ParamConverter(param: "body", type: Blog::Models::Article, converter: Athena::Routing::Converters::RequestBody)]
+    @[Athena::Routing::ParamConverter(param: "body", type: Blog::Models::Article, converter: RequestBody)]
     def update_article(body : Blog::Models::Article) : Blog::Models::Article
       body.user = @user_storage.user
       body.save
@@ -30,13 +30,13 @@ module Blog::Controllers
     end
 
     @[Athena::Routing::Get(path: "/:article_id")]
-    @[Athena::Routing::ParamConverter(param: "article", pk_type: Int64, type: Blog::Models::Article, converter: Athena::Routing::Converters::Exists)]
+    @[Athena::Routing::ParamConverter(param: "article", pk_type: Int64, type: Blog::Models::Article, converter: Exists)]
     def get_article(article : Blog::Models::Article) : Blog::Models::Article
       article
     end
 
     @[Athena::Routing::Delete(path: "/:article_id")]
-    @[Athena::Routing::ParamConverter(param: "article", pk_type: Int64, type: Blog::Models::Article, converter: Athena::Routing::Converters::Exists)]
+    @[Athena::Routing::ParamConverter(param: "article", pk_type: Int64, type: Blog::Models::Article, converter: Exists)]
     def delete_article(article : Blog::Models::Article) : Nil
       article.deleted_at = Time.utc
       article.save
